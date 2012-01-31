a4ad9fdfbf47ff4f58d7c13cbe7e8aba266229cf
Thomas Wiecki <thomas.wiecki@gmail.com>
2012-01-31 09:17:51 -0500

    ENH: Added example setup_integrate.py

---
+++ examples/setup_integrate.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+import os
+
+gsl_include = os.popen('gsl-config --cflags').read()[2:-1]
+
+if gsl_include == '':
+    print "Couldn't find gsl-config. Make sure it's installed and in the path."
+    import sys
+    sys.exit(-1)
+
+setup(
+    name="integrate",
+    version="0.1",
+    author="Thomas V. Wiecki",
+    author_email="thomas_wiecki@brown.edu",
+    url="http://github.com/twiecki/CythonGSL",
+    description="CythonGSL example integrate.",
+    install_requires=['NumPy >=1.3.0'],
+    setup_requires=['NumPy >=1.3.0'],
+    include_dirs = [np.get_include(), gsl_include],
+    cmdclass = {'build_ext': build_ext},
+    classifiers=[
+                'Development Status :: 3 - Alpha',
+                'Environment :: Console',
+                'Operating System :: OS Independent',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: GNU General Public License (GPL)',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering',
+                 ],
+    ext_modules = [Extension("integrate", ["integrate.pyx"], libraries=['gsl','gslcblas'])]
+)
+
